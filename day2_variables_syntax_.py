9d665b31c5e145dd0110b6e10472c597cf30ca52
Shriram9876 <180640987+Shriram9876@users.noreply.github.com>
2025-08-13 18:15:45 +0530

    day 2
    i need to understand the questions bit more. do correct on my mistakes and point better ways do it

---
+++ day2_variables_syntax_.py
@@ -0,0 +1,11 @@
+name = "Alice"
+age = 22
+city = "Paris"
+
+print(f"{name}, {age}, {city}")
+
+age = 23
+
+name = age = city = "updated"
+
+print(f"{name}, \n{age}, \n{city}")
---
+++ day2_variables_syntax_2.py
@@ -0,0 +1,61 @@
+#exercise 1
+first_name = "Jhon"
+last_name = "smith"
+age = 23
+
+print(first_name,last_name,age)
+
+#exercise 2
+score = 10
+print(score)
+
+score = 20
+print(score)
+
+#exercise 3
+pet1 = "katze"
+pet2 = "hund"
+pet3 = "bird"
+
+#or
+pet1, pet2, pet3 = "Katze", "Hund", "Bird"
+
+print(pet1,pet2,pet3)
+
+#exercise 4 
+x,y = 15,30
+x,y = y,x
+
+print(x,y)
+
+#exercise 5
+data = 50
+print(data)
+
+data = "fifty"
+print(data)
+
+data = True
+print(data)
+
+#exercise 6
+name = input("enter your name: ")
+age = int(input('enter your age: '))
+country = input("enter your country's name: ")
+print(f"My name is {name}, I am {age} years old, and I live in {country}.")
+
+#output
+"""
+Jhon smith 23
+10
+20
+Katze Hund Bird
+30 15
+50
+fifty
+True
+enter your name: luka
+enter your age: 20
+enter your country's name: vien
+My name is luka, I am 20 years old, and I live in vien.
+"""
