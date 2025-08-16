24bed2a09892b6be3c799612f31c975a32d215e3
Shriram9876 <180640987+Shriram9876@users.noreply.github.com>
2025-08-16 19:13:00 +0530

    day_4
    i am self learning with the help of A.I

---
+++ day4_expressions_&_operators.py
@@ -0,0 +1,21 @@
+#exercise 1
+print(29%5)
+
+#exercise 2
+print(abs(-456))
+
+#exercise 3
+print(15%3, 15%3)
+
+#exercise 4
+print(divmod(100,7))
+
+#exercise 5
+num = int(input("enter a number: "))
+print(num >= 10 and num <= 20)
+
+#exercise 6
+x = [1,2,3]
+y = [1,2,3]
+print(x == y)
+print(x is y)
